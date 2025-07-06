0c3bc8a252639b730bef99e07068660272fa53dd
Mahmoud Reda <mahmoudreda457@gmail.com>
2025-07-06 22:49:55 +0300

    using next/form

--- Printforge/app/3d-models/page.tsx
+++ Printforge/app/3d-models/page.tsx
@@ -1,9 +1,10 @@
+import Form from 'next/form'
 import ModelsGrid from "@/app/components/ModelsGrid"
 import type { Model, ModelsPageProps } from "@/app/types"
 import { getModels } from "@/app/lib/models"
 
 export default async function Page({ searchParams }: ModelsPageProps) {
-  const { query } = await searchParams
+  const { query } = await searchParams || {}
   const models = await getModels()
 
   const filterdModels = query ? models.filter((model: Model) => {
@@ -14,17 +15,17 @@ export default async function Page({ searchParams }: ModelsPageProps) {
 
   return (
     <>
-      <form>
+      <Form action={'/3d-models'} className="flex items-center justify-center w-full py-4">
         <label className="sr-only">Search for 3d models</label>
         <input
           type="text"
           name="query"
-          placeholder="Search for 3d models"
-          className="w-11/12 px-4 py-2 mt-2 ml-4 border border-gray-300 rounded-md lg:max-w-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="Search for 3d models..."
+          className="w-11/12 py-3 pl-5 pr-5 text-sm placeholder-gray-500 bg-white border border-[#606060] rounded-full focus:border-[#606060] focus:outline-none focus:ring-0 md:text-base"
           defaultValue={query ?? ""}
           autoComplete="off"
         />
-      </form>
+      </Form>
       <ModelsGrid title="3D Models" models={filterdModels} />
     </>
   )
