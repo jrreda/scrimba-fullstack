b0e93424f14188669a62f2c849f77cccc4064863
Reda <mahmoudreda457@gmail.com>
2025-11-13 06:58:28 +0200

    ProtectedRoute

--- Sales Dashboard/src/components/Header.jsx
+++ Sales Dashboard/src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
 
     const { success, error } = await signOut();
     if (success) {
-      navigate("/");
+      navigate("/signin");
     } else {
       setError(error.message);
     }
--- Sales Dashboard/src/router.jsx
+++ Sales Dashboard/src/router.jsx
@@ -1,20 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "./routes/Dashboard";
 import Header from "./components/Header";
-import Signin from "./components/SignIn";
+import Signin from "./components/Signin";
+import RootRedirect from "./routes/RootRedirect";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <RootRedirect />,
+  },
+  {
+    path: "/signin",
     element: <Signin />,
   },
   {
     path: "/dashboard",
     element: (
-      <>
+      <ProtectedRoute>
         <Header />
         <Dashboard />
-      </>
+      </ProtectedRoute>
     ),
   },
 ]);
