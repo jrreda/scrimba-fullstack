d45836eb7f322ed4d30c56b2df2455de16215b46
Reda <ma7moudr3d4@gmail.com>
2025-11-26 18:58:50 +0200

    Authentication & cart

--- Spiral Sounds/seedTable.js
+++ Spiral Sounds/seedTable.js
@@ -1,43 +1,35 @@
-import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
-import path from 'node:path'
-import { vinyl } from './data.js'
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import path from "node:path";
+import { vinyl } from "./data.js";
 
 async function seedTable() {
- 
   const db = await open({
-    filename: path.join('database.db'),
-    driver: sqlite3.Database
-  })
+    filename: path.join("database.db"),
+    driver: sqlite3.Database,
+  });
 
   try {
-
-    await db.exec('BEGIN TRANSACTION')
+    await db.exec("BEGIN TRANSACTION");
 
     for (const { title, artist, price, image, year, genre, stock } of vinyl) {
-
-      await db.run(`
+      await db.run(
+        `
         INSERT INTO products (title, artist, price, image, year, genre, stock)
         VALUES (?, ?, ?, ?, ?, ?, ?)`,
         [title, artist, price, image, year, genre, stock]
-        )
-
+      );
     }
 
-    await db.exec('COMMIT')
-    console.log('All records inserted successfully.')
-
+    await db.exec("COMMIT");
+    console.log("All records inserted successfully.");
   } catch (err) {
-
-    await db.exec('ROLLBACK')
-    console.error('Error inserting data:', err.message)
-
+    await db.exec("ROLLBACK");
+    console.error("Error inserting data:", err.message);
   } finally {
-
-    await db.close()
-    console.log('Database connection closed.')
-    
+    await db.close();
+    console.log("Database connection closed.");
   }
 }
 
-seedTable()
+seedTable();
